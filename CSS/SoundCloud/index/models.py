b555118fc5b0712f947aba650ff4883270ba7b81
imnotmoon <imnotmoon@gmail.com>
2020-08-07 18:09:38 +0900

    2
    2

--- CSS/SoundCloud/index/models.py
+++ CSS/SoundCloud/index/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Sets(models.Model) :
+    title = models.CharField(max_length=20, null=False)
+    name = models.CharField(max_length=20, null=False)
+    cover = models.CharField(max_length=40, default="")
+    genre = models.IntegerField(null=False, default=1)
+
+    def __str__(self) :
+        return self.title
+
